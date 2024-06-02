8f77b549278c21ab3d4e357e7d9db34db74ce147
max burger <mburger89@me.com>
2024-06-02 16:40:02 -0700

    adds concern for article/comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+  
+  has_many :comments
+  
   validates :title, presence: true
-  validates :body, presence: true, lentgh: { minimum: 10}
+  validates :body, presence: true, length: { minimum: 10}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles do 
+    resources :comments
+  end
+  # Define your application routes per the DSL in 
+  # https://guides.rubyonrails.org/routing.html
 end
