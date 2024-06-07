1235f197ef05b30e281680586c1997552a64b540
max burger <mburger89@me.com>
2024-06-07 09:23:48 -0700

    adds rich text package

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,6 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
+  has_rich_text :content
   validates :body, presence: true, length: { minimum: 10}
 end
